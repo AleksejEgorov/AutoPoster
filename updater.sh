009311fefc5b0696b66217bd29796c24f15df3c1
Aleksej Egorov <Egorov.Aleksej.V@outlook.com>
2025-01-19 00:46:56 +0300

    Fix redundant sudo command in updater script

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/autoposter
 sudo -u botrunner git pull --rebase
-sudo sudo -u botrunner /opt/autoposter/.venv/bin/pip install -r requirements.txt
+sudo -u botrunner /opt/autoposter/.venv/bin/pip install -r requirements.txt
 chown botrunner:botrunner /opt/autoposter -r
 systemctl restart autoposter
 systemctl status autoposter
