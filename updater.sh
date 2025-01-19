effdf2ebc1ef1798bc3ef3fac79bc62ea25578e5
Aleksej Egorov <Egorov.Aleksej.V@outlook.com>
2025-01-19 14:55:15 +0300

    Fix chown command to use recursive flag in updater script

--- updater.sh
+++ updater.sh
@@ -2,6 +2,6 @@
 cd /opt/autoposter
 sudo -u botrunner git pull --rebase
 sudo -u botrunner /opt/autoposter/.venv/bin/pip install -r requirements.txt
-chown botrunner:botrunner /opt/autoposter -r
+chown botrunner:botrunner /opt/autoposter -R
 systemctl restart autoposter
 systemctl status autoposter
