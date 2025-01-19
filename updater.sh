62d18b75c3f319dec607c40043c8240c8018c5c0
root@VPS-VPN-01.apt-1088.local <root@VPS-VPN-01.apt-1088.local>
2025-01-19 19:03:37 +0100

    Don't use pip cache in updater

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/autoposter
 sudo -u botrunner git pull --rebase
-sudo -u botrunner /opt/autoposter/.venv/bin/pip install -r requirements.txt
+sudo -u botrunner /opt/autoposter/.venv/bin/pip install -r requirements.txt --no-cache-dir
 chown botrunner:botrunner /opt/autoposter -R
 systemctl restart autoposter
 systemctl status autoposter
