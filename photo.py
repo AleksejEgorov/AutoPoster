768dff27167bcecb435a8d6a6f7b36fe21ab1468
Aleksej Egorov <Egorov.Aleksej.V@outlook.com>
2025-10-11 14:32:16 +0300

    Implement retry logic for tag retrieval in Photo class

--- photo.py
+++ photo.py
@@ -3,6 +3,7 @@ This class describes photo in post
 '''
 import logging
 import requests
+from time import sleep
 from PIL import Image, ImageOps
 
 logger = logging.getLogger(__name__)
@@ -42,19 +43,52 @@ class Photo:
 
         logger.debug(print('Processing photo %s', self.__id))
 
-        with open(self.__file_path, 'rb') as image:
-            tag_response = requests.post(
-                'https://api.imagga.com/v2/tags',
-                auth=immaga_auth,
-                files={'image': image},
-                timeout=config['imagga']['timeout']
-            )
+        attempts_left = 3
+        need_retry = True
+        tag_response = None
 
-        logger.info(
-            '%s tags received for photo %s',
-            len(tag_response.json()['result'].get('tags')),
-            self.__id
-        )
+        while attempts_left > 0 and need_retry:
+            attempts_left -= 1
+            try:
+                with open(self.__file_path, 'rb') as image:
+                    tag_response = requests.post(
+                        'https://api.imagga.com/v2/tags',
+                        auth=immaga_auth,
+                        files={'image': image},
+                        timeout=config['imagga']['timeout']
+                    )
+
+                logger.info(
+                    '%s tags received for photo %s',
+                    len(tag_response.json()['result'].get('tags')),
+                    self.__id
+                )
+            except requests.exceptions.Timeout:
+                logger.warning(
+                    'Timeout error receiving tags for photo %s, attempts left %s',
+                    self.__id,
+                    attempts_left
+                )
+                if attempts_left == 0:
+                    logger.error('All attempts to receive tags for photo %s failed', self.__id)
+                    raise
+                sleep(30)
+            except Exception as err:
+                logger.error(
+                    '%s Error receiving tags for photo %s: %s',
+                    type(err),
+                    self.__id,
+                    err
+                )
+                raise
+            need_retry = False
+
+        if tag_response is None:
+            logger.error(
+                'Error receiving tags for photo %s: %s',
+                self.__id,
+            )
+            raise
 
         photo_tags = tag_response.json()['result'].get('tags')
         tags_string = ', '.join(
